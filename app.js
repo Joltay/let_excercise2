52806565771a26a2c9bcc6deb91143f3ca0ca702
Joltay <5202302@gmail.com>
2019-10-17 14:10:09 +0600

    Excercise2
    Excercise2

---
+++ app.js
@@ -0,0 +1,7 @@
+
+let Hello = 'Всем привет!';
+let fName = 'Болат';
+let lName = 'Жолтай';
+let object = 'JavaScript';
+let result = Hello + ' Меня зовут ' + fName + ' ' + lName + ', я изучаю ' + object + '.';
+alert(result);
